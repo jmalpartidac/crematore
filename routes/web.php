09d844f19b92263db6238089efada9a1f6aeefd9
Jose Malpartida <ing.jmalpartidac@gmail.com>
2018-02-08 15:43:20 -0500

    mi primer commit

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+Route::get('/', function() {
+	return 'hola';
+});
+
+Route::get('/usuario', function() {
+	return 'usuarios';
+});
+
+Route::get('/usuario/{id}', function($id) {
+	return "usuario con id {$id}";
+})->where('id', '[0-9]+');
+
+Route::get('/usuario/nuevo', function() {
+	return 'crear nuevo usuario';
+});
+
+Route::get('broma/{nombre}/{apodo?}', function($nombre, $apodo = null) {
+
+	if ($apodo) {
+		return "hola {$nombre}, tu apodo es {$apodo}";
+	}else{
+		return "hola {$nombre}, no tienes apodo";
+	}
+});
