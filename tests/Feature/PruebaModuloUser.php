e538e3a1d19d1a43b06c69609f2083b66b3fb66d
Jose Malpartida <ing.jmalpartidac@gmail.com>
2018-02-13 09:00:00 -0500

    mi quinto commit

--- tests/Feature/PruebaModuloUser.php
+++ tests/Feature/PruebaModuloUser.php
@@ -8,18 +8,18 @@ use Illuminate\Foundation\Testing\RefreshDatabase;
 
 class PruebaModuloUser extends TestCase
 {
-    /** @test */
-    function it_loads_the_users_list_page()
+    /** @return void */
+    function test_it_loads_the_users_list_page()
     {
-        $this->get('usuarios')
+        $this->get('/usuarios')
         	->assertStatus(200)
         	->assertSee('usuarios');
     }
 
-    /** @test */
-    function it_loads_the_users_details_page()
+    /** @return void */
+    function test_it_loads_the_users_details_page()
     {
-        $this->get('usuarios/5')
+        $this->get('/usuarios/5')
         	->assertStatus(200)
         	->assertSee('Mostrando detalle del usuario: 5');
     }
